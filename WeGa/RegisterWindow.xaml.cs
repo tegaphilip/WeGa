c214bc97630998b08ad927bc4e3c522389c31ca1
unknown <w.wu.15@INFO-69-11.intranet.int>
2016-05-26 18:19:09 +0200

    Created a RegisterWindow with simple validation.

---
+++ WeGa/RegisterWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WeGa
+{
+    /// <summary>
+    /// Interaction logic for RegisterWindow.xaml
+    /// </summary>
+    public partial class RegisterWindow : Window
+    {
+        public RegisterWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            message.Content = "";
+
+            if (nickname.Text.Equals(""))
+            {
+                message.Content += "Please enter your nickname~~ \n";
+            }
+            if (username.Text.Equals(""))
+            {
+                message.Content += "Please enter your username~~ \n";
+            }
+            if (password.Password.Equals(""))
+            {
+                message.Content += "Please enter your password~~ \n";
+            }
+            if (passwordRepeat.Password.Equals(""))
+            {
+                message.Content += "Repeat your password please~~ \n";
+            }
+            if (password.Password != passwordRepeat.Password)
+            {
+                message.Content += "Your passwords are not the same! \n";
+            }
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            nickname.Clear();
+            username.Clear();
+            password.Clear();
+            passwordRepeat.Clear();
+        }
+    }
+}
