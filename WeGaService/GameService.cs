b6f450a48dbd17ecfdec09b5e70b1eab6ce8de7d
tegaphilip <tega.philip@gmail.com>
2016-06-02 16:32:00 +0200

    Added service to create a game

--- WeGaService/GameService.cs
+++ WeGaService/GameService.cs
@@ -17,22 +17,22 @@ namespace WeGaService
 
         public Dictionary<String, String> Login(string username, string password)
         {
-            Dictionary<String, String> user = new Dictionary<string, string>();
-            user["status"] = ERROR;
+            Dictionary<String, String> response = new Dictionary<string, string>();
+            response["status"] = ERROR;
 
             var p = new DBConn().login(username, password);
             if (p == null)
             {
-                user["message"] = DBConn.getLatestErrorMessage();
+                response["message"] = DBConn.getLatestErrorMessage();
             }
             else
             {
-                user["status"] = SUCCESS;
-                user["nickname"] = p.nickname;
-                user["username"] = p.username;
+                response["status"] = SUCCESS;
+                response["nickname"] = p.nickname;
+                response["username"] = p.username;
             }
 
-            return user;
+            return response;
         }
 
         public bool RegisterPlayer(string username, string nickname, string password)
@@ -43,6 +43,22 @@ namespace WeGaService
         public List<string> getPlayersNm()
         {
             return new DBConn().getPlayersNm();
+        }
+
+
+        public Dictionary<String, String> CreateGame(string SenderNickName, string ReceiverNickName, string Letters)
+        {
+            Dictionary<String, String> response = new Dictionary<string, string>();
+            response["status"] = ERROR;
+            if (new DBConn().CreateGame(SenderNickName, ReceiverNickName, Letters))
+            {
+                response["status"] = SUCCESS;
+            }
+            else
+            {
+                response["message"] = DBConn.getLatestErrorMessage();
+            }
+            return response;
         }
     }
 }
